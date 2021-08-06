9d9a3a836f352cbedbc572c58e4886ef3a00d8e3
NetPatrol <madraddog@gmail.com>
2021-08-06 14:24:33 +0300

    fix commit

--- src/main/resources/static/getUserInfo.js
+++ src/main/resources/static/getUserInfo.js
@@ -15,26 +15,27 @@ const getUserByLogin = async function(login) {
         .then(res => res.json())
         .then(data => {
             navBarInfo +=
-               `<a href='/logout' class='btn btn-primary btn-sm' role='button'>Sign out</a>
+                `<a href='/logout' class='btn btn-primary btn-sm' role='button'>Sign out</a>
                 <ul class="navbar-nav flex-row">
-                    <li class="nav-item me-3 me-lg-1 text-white">
-                        <a class="nav-link d-sm-flex align-items-sm-center" href="#">
-                            <img src="${data.linkAvatar}" class="rounded-circle" height="22" alt="" loading="lazy"/>`
+                <li class="nav-item me-3 me-lg-1 text-white">
+                <a class="nav-link d-sm-flex align-items-sm-center" href="#">
+                <img src="${data.linkAvatar}" class="rounded-circle" height="22" alt="" loading="lazy"/>`
             navBarInfo += `<span class="p-1">${getDataTime(data.name)}</span>`
             data.roles.forEach(r => {
                 if (r.role === 'ROLE_ADMIN') {
-                    navBarInfo += ` <span class="p-1">статус: администратор</span>
+                    navBarInfo +=
+                        ` <span class="p-1">статус: администратор</span>
                         </a>
-                    </li>
-                </ul>`
+                        </li>
+                        </ul>`
                 } else if (r.role === 'ROLE_USER') {
-                    navBarInfo += ` <span class="p-1">статус: пользователь</span>
+                    navBarInfo +=
+                        ` <span class="p-1">статус: пользователь</span>
                         </a>
-                    </li>
-                </ul>`
+                        </li>
+                        </ul>`
                 }
             })
-
             navBar.innerHTML = navBarInfo
 
             uInfo +=
@@ -42,27 +43,27 @@ const getUserByLogin = async function(login) {
                 <img src = "${data.linkAvatar}" height = "190" alt = "" loading = "lazy" / >
                 </div>
                 <div class="col-md-6">
-                    <div class="card-body">
-                        <h6 class="card-title">${data.name}  ${data.lastName}</h6>
-                            <div class="mt-3">
-                            <p>Date birthday: ${data.birthday}</p>
-                            <p>City: ${data.city}</p>
-                            <p>Workplace: ${data.workplace}</p>
-                        </div>
-                    </div>
+                <div class="card-body">
+                <h6 class="card-title">${data.name}  ${data.lastName}</h6>
+                <div class="mt-3">
+                <p>Date birthday: ${data.birthday}</p>
+                <p>City: ${data.city}</p>
+                <p>Workplace: ${data.workplace}</p>
+                </div>
+                </div>
                 </div>`
                 userInfo.innerHTML = uInfo
 
             data.articles.forEach(a => {
                 article +=
                     `<div class='card mt-3'>
-                        <div class='card-body'>
-                            <h5 class='card-title'>${a.title}</h5>
-                            <p class='card-text'>${a.article}</p>
-                            <p class='card-text'>
-                                <small class='text-muted'>${a.date}</small>
-                            </p>
-                        </div>
+                    <div class='card-body'>
+                    <h5 class='card-title'>${a.title}</h5>
+                    <p class='card-text'>${a.article}</p>
+                    <p class='card-text'>
+                    <small class='text-muted'>${a.date}</small>
+                    </p>
+                    </div>
                     </div>`
                 articles.innerHTML = article
             })
